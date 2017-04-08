d4e43f98fdb4a5c59496ae8d5b3ff5793c67a42b
yangfei <yfei89@126.com>
2017-04-08 16:09:36 +0800

    quick sort

---
+++ quick_sort.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+'''
+Θ(nlgn)
+'''
+import random
+
+def quick_sort(a,start,end):
+	if start < end:
+		flag 	= a[start]
+		mid 	= start
+		for i in range(start+1, end):
+			if a[i] < flag:
+				a[mid],a[i] = a[i],a[mid]
+				mid 	+= 1
+		a[mid] = flag
+		quick_sort(a, start, mid)
+		quick_sort(a, mid+1, end)
+	return a
+
+
+x = []
+for k in range(100):
+	x.append(random.randint(0, 100))
+print quick_sort(x, 0, 100)
