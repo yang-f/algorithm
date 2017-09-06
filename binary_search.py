3db9fc063fe5d0dc6fabc7c2542d8c38a683862e
yangfei <yfei89@126.com>
2017-09-06 15:38:24 +0800

    binary_search

--- binary_search.py
+++ binary_search.py
@@ -2,12 +2,16 @@
 '''
 Θ(lgn)
 '''
-def binary_search(a,key):
-	if a[int(len(a)/2)] == key:
-		return int(len(a)/2)
-	elif a[int(len(a)/2)] > key:
-		return binary_search(a[:int(len(a)/2)],key)
-	else:
-		return binary_search(a[int(len(a)/2):],key)
+def binary_search(a,start,end,key):
+    if start==end:
+        return -1
+    midle = start + int((end - start)/2);
+    if a[midle] == key:
+        return midle
+    elif a[midle] > key:
+            return binary_search(a,start,midle,key)
+    else:
+        return binary_search(a,midle+1,end,key)
 
-print binary_search([1,3,4,5,6,77],3)
+a = [1,3,4,5,6,77]
+print binary_search(a,0,len(a)-1,6)
